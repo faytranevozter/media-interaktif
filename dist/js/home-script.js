e9e0bef84155546c729812d788cb00a29094df27
Fahrur Rifai <faimaknyus@gmail.com>
2017-06-13 18:42:34 +0700

    initial commit

---
+++ dist/js/home-script.js
@@ -0,0 +1,15 @@
+/*$(document).ready(function(){
+	$("#port-slider").flexslider({
+		selector: ".port-slide-box > .port-slide",
+		animation: "slide",
+		// useCSS: false,
+		pausePlay: true,
+		controlNav: false,
+		slideshowSpeed: 10000,
+		// animationSpeed: 2000,
+		// direction: "vertical",
+		smoothHeight:true,
+		// directionNav: false
+	});
+
+});*/
